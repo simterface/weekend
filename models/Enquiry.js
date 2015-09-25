9666db09c3cf21f0f076249121d8114acc52f8eb
Vasily Belolapotkov <belolapotkov.v@gmail.com>
2015-09-26 00:52:21 +0300

    Initial commit

---
+++ models/Enquiry.js
@@ -0,0 +1,31 @@
+var keystone = require('keystone');
+var Types = keystone.Field.Types;
+
+/**
+ * Enquiry Model
+ * =============
+ */
+
+var Enquiry = new keystone.List('Enquiry', {
+	nocreate: true,
+	noedit: true
+});
+
+// TODO move enquery types to config ?
+Enquiry.add({
+	name: { type: Types.Name, required: true },
+	email: { type: Types.Email, required: true },
+	phone: { type: String },
+	enquiryType: { type: Types.Select, options: [
+		{ value: 'message', label: 'Просто оставить сообщение' },
+		{ value: 'question', label: 'Есть вопрос' },
+		{ value: 'invitation', label: 'Пригласить на мероприятие' },
+		{ value: 'other', label: 'Другое...' }
+	] },
+	message: { type: Types.Markdown, required: true },
+	createdAt: { type: Date, default: Date.now }
+});
+
+Enquiry.defaultSort = '-createdAt';
+Enquiry.defaultColumns = 'name, email, enquiryType, createdAt';
+Enquiry.register();
---
+++ routes/views/playlist.js
@@ -0,0 +1,13 @@
+var keystone = require('keystone');
+
+exports = module.exports = function(req, res) {
+
+	var view = new keystone.View(req, res);
+	var locals = res.locals;
+
+	// Set locals
+	locals.section = 'playlist';
+
+	// Render the view
+	view.render('playlist');
+};
