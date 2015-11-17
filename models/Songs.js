6a4752cc46a96accc65baac32dc38256bf4f4710
Vasily Belolapotkov <belolapotkov.v@gmail.com>
2015-11-18 00:25:12 +0300

    Added basic playlist layout and styling

--- models/Songs.js
+++ models/Songs.js
@@ -12,24 +12,12 @@ var Songs = new keystone.List('Songs', {
   plural: 'Песни',
   defaultSort: '-singer',
   sortable: true,
-  defaultColumns: 'singer, name, album'
+  defaultColumns: 'singer, name'
 });
 
 Songs.add({
 	name: { type: Types.Text, initial: true, required: true, label: 'Название' },
-	singer: { type: Types.Text, initial: true, required: true, label: 'Исполнитель' },
-	album: { type: String, label: 'Альбом' },
-	year: { type: Types.Number, label: 'Год' },
-  file: {
-    type: Types.LocalFile,
-    label: 'Файл (.m4a)',
-    dest: 'public/audio',
-    allowedTypes: [
-      'audio/x-m4a'
-      // 'audio/aac',
-      // 'audio/mpeg'
-    ]
-  }
+	singer: { type: Types.Text, initial: true, required: true, label: 'Исполнитель' }
 });
 
 Songs.register();
